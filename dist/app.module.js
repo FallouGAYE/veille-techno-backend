0db30e52721695c24dd2fafe18b70cb56db91eaa
Fallou Gaye <fallougayetaif200@gmail.com>
2026-09-23 12:25:44 +0200

    Set up core modules, Swagger and Prisma

--- dist/app.module.js
+++ dist/app.module.js
@@ -10,12 +10,16 @@ exports.AppModule = void 0;
 const common_1 = require("@nestjs/common");
 const app_controller_1 = require("./app.controller");
 const app_service_1 = require("./app.service");
+const users_module_1 = require("./users/users.module");
+const auth_module_1 = require("./auth/auth.module");
+const lists_module_1 = require("./lists/lists.module");
+const cards_module_1 = require("./cards/cards.module");
 let AppModule = class AppModule {
 };
 exports.AppModule = AppModule;
 exports.AppModule = AppModule = __decorate([
     (0, common_1.Module)({
-        imports: [],
+        imports: [users_module_1.UsersModule, auth_module_1.AuthModule, lists_module_1.ListsModule, cards_module_1.CardsModule],
         controllers: [app_controller_1.AppController],
         providers: [app_service_1.AppService],
     })
--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
+import { ListsModule } from './lists/lists.module';
+import { CardsModule } from './cards/cards.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, AuthModule, ListsModule, CardsModule],
   controllers: [AppController],
   providers: [AppService],
 })
